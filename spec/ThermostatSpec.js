98bf72610dc1ba406a3da22229fc9ab9425742ef
Alexa <alexakearns@outlook.com>
2020-05-23 15:20:26 +0100

    add switch power saving mode back on - Red

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -37,4 +37,11 @@ describe('Thermostat', function() {
         thermostat.switchPowerSavingModeOff();
         expect(thermostat.isPowerSavingModeOn()).toBe(false);
     });
+
+    it('can swtich power saving mode back on', function() {
+        thermostat.switchPowerSavingModeOff();
+        expect(thermostat.isPowerSavingModeOn()).toBe(false);
+        thermostat.switchPowerSavingModeOn();
+        expect(thermostat.isPowerSavingModeOn()).toBe(true);
+    });
 });
